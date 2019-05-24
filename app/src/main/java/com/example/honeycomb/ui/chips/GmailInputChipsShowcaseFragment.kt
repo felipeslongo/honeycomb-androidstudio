dcc162980a9bc13dc9e0d0018c8a1846785b3d7b
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-05-24 17:08:50 -0300

    Implementando chipgrou

--- app/src/main/java/com/example/honeycomb/ui/chips/GmailInputChipsShowcaseFragment.kt
+++ app/src/main/java/com/example/honeycomb/ui/chips/GmailInputChipsShowcaseFragment.kt
@@ -1,13 +1,17 @@
 package com.example.honeycomb.ui.chips
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.honeycomb.R
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class GmailInputChipsShowcaseFragment : Fragment() {
 
@@ -15,19 +19,29 @@ class GmailInputChipsShowcaseFragment : Fragment() {
         fun newInstance() = GmailInputChipsShowcaseFragment()
     }
 
+    private lateinit var root: ChipGroup
     private lateinit var viewModel: GmailInputChipsShowcaseViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.gmail_input_chips_showcase_fragment, container, false)
+        val view = inflater.inflate(R.layout.gmail_input_chips_showcase_fragment, container, false)
+
+        root = view.findViewById(R.id.gmail_input_chips_showcase)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(GmailInputChipsShowcaseViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+        for (option in viewModel.options) {
+            val chip = Chip(root.context)
+            chip.text = "Chip Chip!"
+            root.addView(chip,ChipGroup.LayoutParams.WRAP_CONTENT, ChipGroup.LayoutParams.WRAP_CONTENT)
+        }
+
+    }
 }
--- app/src/main/java/com/example/honeycomb/ui/chips/GmailInputChipsShowcaseViewModel.kt
+++ app/src/main/java/com/example/honeycomb/ui/chips/GmailInputChipsShowcaseViewModel.kt
@@ -3,5 +3,5 @@ package com.example.honeycomb.ui.chips
 import androidx.lifecycle.ViewModel
 
 class GmailInputChipsShowcaseViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val options : List<String> = listOf("Bokoblin", "Moblin", "Darknut", "Octorok", "Stalfos", "Wallmaster")
 }
