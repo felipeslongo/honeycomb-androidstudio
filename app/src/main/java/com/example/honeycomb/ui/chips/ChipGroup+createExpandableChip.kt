d4ff50f4341e39091f06cf586f2e54595c376d4f
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-05-27 10:28:35 -0300

    Using ContextThemeWrapper for view construction

--- app/src/main/java/com/example/honeycomb/ui/chips/ChipGroup+createExpandableChip.kt
+++ app/src/main/java/com/example/honeycomb/ui/chips/ChipGroup+createExpandableChip.kt
@@ -1,11 +1,13 @@
 package com.example.honeycomb.ui.chips
 
 import android.view.LayoutInflater
+import androidx.appcompat.view.ContextThemeWrapper
+import com.example.honeycomb.R
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 
 fun ChipGroup.createExpandableChip(text: String, layoutInflater: LayoutInflater, removeOnCloseClicked: (Chip) -> Boolean) : Chip{
-    val chip = Chip(this.context)
+    val chip = Chip(ContextThemeWrapper(context, R.style.Widget_MaterialComponents_Chip_Entry), null, 0)
     chip.text = text
     chip.isClickable = true
     chip.isCheckable = false
