a5b755b4f0ce5b39f4c032e654a96ab52b25de0b
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-08 18:00:44 -0300

    Implementing setupDrawerNavigationView

--- app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationViewShowcaseActivity.kt
+++ app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationViewShowcaseActivity.kt
@@ -3,7 +3,10 @@ package com.example.honeycomb.ui.navigationViews
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.honeycomb.R
+import com.google.android.material.navigation.NavigationView
 
 class NavigationViewShowcaseActivity : AppCompatActivity() {
 
@@ -16,5 +19,16 @@ class NavigationViewShowcaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation_view_showcase)
+        bindDrawerNavigationView()
+    }
+
+    private fun bindDrawerNavigationView() {
+        val drawer = findViewById<DrawerLayout>(R.id.navigation_view_showcase_drawer)
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view_showcase_drawer_navigation)
+        navigationView.setNavigationItemSelectedListener {menuItem: MenuItem ->
+            menuItem.isChecked = true
+            drawer.closeDrawers()
+            true
+        }
     }
 }
