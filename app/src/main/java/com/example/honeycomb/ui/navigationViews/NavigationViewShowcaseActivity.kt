ce480c84ac8104e346fca5d2af4d1f4192a81dab
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-09 02:10:21 -0300

    Implementing Custom Switch in navigation view

--- app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationViewShowcaseActivity.kt
+++ app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationViewShowcaseActivity.kt
@@ -3,12 +3,18 @@ package com.example.honeycomb.ui.navigationViews
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.honeycomb.R
 import com.google.android.material.navigation.NavigationView
+import android.widget.Toast
+import com.google.android.material.switchmaterial.SwitchMaterial
+
 
 class NavigationViewShowcaseActivity : AppCompatActivity() {
+
+    private val LogTag: String = NavigationViewShowcaseActivity::class.java.toString()
 
     companion object{
         fun present(caller: AppCompatActivity){
@@ -25,10 +31,20 @@ class NavigationViewShowcaseActivity : AppCompatActivity() {
     private fun bindDrawerNavigationView() {
         val drawer = findViewById<DrawerLayout>(R.id.navigation_view_showcase_drawer)
         val navigationView = findViewById<NavigationView>(R.id.navigation_view_showcase_drawer_navigation)
+
         navigationView.setNavigationItemSelectedListener {menuItem: MenuItem ->
             menuItem.isChecked = true
             drawer.closeDrawers()
             true
+        }
+
+        val item = navigationView.menu
+            .findItem(R.id.navigation_view_showcase_checkable_behavior_actions_switch)
+            .actionView as SwitchMaterial
+        item.setOnCheckedChangeListener { _, isChecked ->
+            val msg = "onCheckedChanged$isChecked"
+            Log.v(LogTag, msg)
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         }
     }
 }
