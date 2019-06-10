e8e5c96c57993d091e5b4bfa02817cdad442ab8b
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-10 00:13:59 -0300

    Implementing onProfilesToggled addProfiles

--- app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationViewShowcaseActivity.kt
+++ app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationViewShowcaseActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.honeycomb.R
@@ -15,6 +16,7 @@ import com.google.android.material.switchmaterial.SwitchMaterial
 class NavigationViewShowcaseActivity : AppCompatActivity() {
 
     private val LogTag: String = NavigationViewShowcaseActivity::class.java.toString()
+    private lateinit var navigationView: NavigationView
 
     companion object{
         fun present(caller: AppCompatActivity){
@@ -30,12 +32,10 @@ class NavigationViewShowcaseActivity : AppCompatActivity() {
 
     private fun bindDrawerNavigationView() {
         val drawer = findViewById<DrawerLayout>(R.id.navigation_view_showcase_drawer)
-        val navigationView = findViewById<NavigationView>(R.id.navigation_view_showcase_drawer_navigation)
+        navigationView = findViewById<NavigationView>(R.id.navigation_view_showcase_drawer_navigation)
 
         val headerView = NavigationHeaderProfileView.create(navigationView)
-        headerView.setOnProfilesToggledListener {isOpen ->
-            Toast.makeText(this, "setOnProfilesToggledListener$isOpen", Toast.LENGTH_SHORT).show()
-        }
+        headerView.setOnProfilesToggledListener {isOpen -> onProfilesToggled(isOpen) }
 
         navigationView.setNavigationItemSelectedListener {menuItem: MenuItem ->
             menuItem.isChecked = true
@@ -53,4 +53,20 @@ class NavigationViewShowcaseActivity : AppCompatActivity() {
         }
     }
 
+    private fun onProfilesToggled(isOpen: Boolean){
+        Toast.makeText(this, "setOnProfilesToggledListener$isOpen", Toast.LENGTH_SHORT).show()
+
+        navigationView.menu.clear()
+        if(isOpen){
+            addProfiles(navigationView.menu)
+            return
+        }
+
+        navigationView.inflateMenu(R.menu.navigation_view_showcase)
+    }
+
+    private fun addProfiles(menu: Menu) {
+        val profile = menu.add("Felipe de Souza Longo")
+        profile.setIcon(R.drawable.baseline_arrow_drop_up_white_48)
+    }
 }
