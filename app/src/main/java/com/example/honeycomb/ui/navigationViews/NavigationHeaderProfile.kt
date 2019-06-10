778bd7582eef3fc2937f1e63caea40cdd8893866
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-09 23:32:14 -0300

    Creating NavigationHeaderProfile

---
+++ app/src/main/java/com/example/honeycomb/ui/navigationViews/NavigationHeaderProfile.kt
@@ -0,0 +1,27 @@
+package com.example.honeycomb.ui.navigationViews
+
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatTextView
+import com.example.honeycomb.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.navigation_header_profile.view.*
+
+class NavigationHeaderProfile private constructor(navigationView: NavigationView) {
+    private val profilesView: AppCompatImageButton
+    private val emailView: AppCompatTextView
+    private val usernameView: AppCompatTextView
+
+    companion object{
+        val LayoutId = R.layout.navigation_header_profile
+
+        fun Create(navigationView: NavigationView) : NavigationHeaderProfile{
+            return NavigationHeaderProfile(navigationView)
+        }
+    }
+        init {
+            val header = navigationView.getHeaderView(0)
+            usernameView = header.navigation_header_profile_username
+            emailView = header.navigation_header_profile_email
+            profilesView = header.navigation_header_profile_profiles
+        }
+}
