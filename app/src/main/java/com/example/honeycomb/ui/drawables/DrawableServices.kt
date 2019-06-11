c97309196693d8f7604fab9a6db5c1d83013183c
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-11 19:00:52 -0300

    Update DrawableServices.kt

--- app/src/main/java/com/example/honeycomb/ui/drawables/DrawableServices.kt
+++ app/src/main/java/com/example/honeycomb/ui/drawables/DrawableServices.kt
@@ -1,6 +1,7 @@
 package com.example.honeycomb.ui.drawables
 
 import android.content.res.Resources
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
@@ -9,7 +10,12 @@ import com.example.honeycomb.R
 class DrawableServices {
     companion object{
         fun createAvatar(resources: Resources, drawableId: Int) : Drawable{
-            val bitmap = BitmapFactory.decodeResource(resources, drawableId)
+            var bitmap = BitmapFactory.decodeResource(resources, drawableId)
+
+//            //https://stackoverflow.com/questions/15789049/crop-a-bitmap-image
+//            val size = Math.min(bitmap.height, bitmap.width)
+//            bitmap = Bitmap.createBitmap(bitmap, 0,0,size, size)
+
             val roundedDrawable = RoundedBitmapDrawableFactory.create(resources, bitmap)
             roundedDrawable.isCircular = true
             roundedDrawable.setAntiAlias(true)
