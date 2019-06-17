425d39433b7d8e8464153c10b5c6e0ee48aaa5ce
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-17 02:03:06 -0300

    Implementing navigation to AndroidDataBindingCodelabActivity

--- app/src/main/java/com/example/honeycomb/ui/dataBindings/AndroidDataBindingCodelab.kt
+++ app/src/main/java/com/example/honeycomb/ui/dataBindings/AndroidDataBindingCodelab.kt
@@ -1,10 +1,18 @@
 package com.example.honeycomb.ui.dataBindings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.honeycomb.R
+import com.example.honeycomb.ui.navigationViews.NavigationViewShowcaseActivity
 
 class AndroidDataBindingCodelab : AppCompatActivity() {
+
+    companion object{
+        fun present(caller: AppCompatActivity){
+            caller.startActivity(Intent(caller, AndroidDataBindingCodelab::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
