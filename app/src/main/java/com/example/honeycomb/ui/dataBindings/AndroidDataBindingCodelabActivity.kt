1c48fde8e7e77429569ee43781157490198c2f9f
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-18 20:34:19 -0300

    Observing data

--- app/src/main/java/com/example/honeycomb/ui/dataBindings/AndroidDataBindingCodelabActivity.kt
+++ app/src/main/java/com/example/honeycomb/ui/dataBindings/AndroidDataBindingCodelabActivity.kt
@@ -54,51 +54,6 @@ class AndroidDataBindingCodelabActivity : AppCompatActivity() {
         setContentView(R.layout.activity_android_data_binding_codelab)
         val binding: ActivityAndroidDataBindingCodelabBinding = DataBindingUtil.setContentView(this, R.layout.activity_android_data_binding_codelab)
         binding.viewmodel = viewModel
-
-        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateLikes()
-    }
-
-    /**
-     * This method has many problems:
-     * - It's calling findViewById multiple times
-     * - It has untestable logic
-     * - It's updating two views when called even if they're not changing
-     */
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        val color = getAssociatedColor(viewModel.popularity, this)
-
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/example/honeycomb/ui/dataBindings/AndroidDataBindingCodelabViewModel.kt
+++ app/src/main/java/com/example/honeycomb/ui/dataBindings/AndroidDataBindingCodelabViewModel.kt
@@ -1,33 +1,39 @@
 package com.example.honeycomb.ui.dataBindings
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
  * A simple VM for [com.example.android.databinding.basicsample.ui.PlainOldActivity].
  */
 class AndroidDataBindingCodelabViewModel : ViewModel() {
-    val name = "Grace"
-    val lastName = "Hopper"
-    var likes = 0
+    private val _name = MutableLiveData<String>().apply { value = "Felipe" }
+    private val _lastName = MutableLiveData<String>().apply { value = "de Souza Longo" }
+    private val _likes = MutableLiveData<Int>().apply { value = 0 }
+
+    val name: LiveData<String> = _name
+    val lastName: LiveData<String> = _lastName
+    var likes: LiveData<Int> = _likes
         private set // This is to prevent external modification of the variable.
 
     /**
      * Increments the number of likes.
      */
     fun onLike() {
-        likes++
+        _likes.value = (likes.value ?: 0) + 1
     }
 
     /**
      * Returns popularity in buckets: [Popularity.NORMAL], [Popularity.POPULAR] or [Popularity.STAR]
      */
-    val popularity: Popularity
-        get() {
-            return when {
-                likes > 9 -> Popularity.STAR
-                likes > 4 -> Popularity.POPULAR
-                else -> Popularity.NORMAL
-            }
+    val popularity: LiveData<Popularity> = Transformations.map(_likes){
+        when {
+            it > 9 -> Popularity.STAR
+            it > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
         }
+    }
 }
 
 enum class Popularity {
