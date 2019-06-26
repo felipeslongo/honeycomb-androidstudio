5a508789405e1b76b6af9ea5520662b246b5cc61
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-26 14:28:18 -0300

    Criando layouts iniciais e activity

---
+++ app/src/main/java/com/example/honeycomb/ui/googlePlay/SincronizacaoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.honeycomb.ui.googlePlay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.honeycomb.R
+
+class SincronizacaoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sincronizacao)
+    }
+}
