f1a923e89ed8e13b888a4bc7224d349dfed2f268
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-26 19:48:36 -0300

    Refactoring DrawableServices

--- app/src/main/java/com/example/honeycomb/ui/drawables/DrawableServices.kt
+++ app/src/main/java/com/example/honeycomb/ui/drawables/DrawableServices.kt
@@ -4,6 +4,7 @@ import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.example.honeycomb.R
 
@@ -12,15 +13,30 @@ class DrawableServices {
         fun createAvatar(resources: Resources, drawableId: Int) : Drawable{
             var bitmap = BitmapFactory.decodeResource(resources, drawableId)
 
-//            //https://stackoverflow.com/questions/15789049/crop-a-bitmap-image
-//            val size = Math.min(bitmap.height, bitmap.width)
-//            bitmap = Bitmap.createBitmap(bitmap, 0,0,size, size)
+            bitmap = createSquare(bitmap)
+            val roundedDrawable = createCircle(resources, bitmap)
 
+            return roundedDrawable
+        }
+
+        private fun createCircle(resources: Resources, bitmap: Bitmap?): RoundedBitmapDrawable {
             val roundedDrawable = RoundedBitmapDrawableFactory.create(resources, bitmap)
             roundedDrawable.isCircular = true
             roundedDrawable.setAntiAlias(true)
-
             return roundedDrawable
+        }
+
+        fun createSquare(bitmap: Bitmap): Bitmap {
+            //https://stackoverflow.com/questions/15789049/crop-a-bitmap-image
+            val size = Math.min(bitmap.height, bitmap.width)
+            var x = bitmap.width - size
+            var y = bitmap.height - size
+            if (x > 0)
+                x /= 2
+            if (y > 0)
+                y /= 2
+
+            return Bitmap.createBitmap(bitmap, x, y, size, size)
         }
     }
 }
