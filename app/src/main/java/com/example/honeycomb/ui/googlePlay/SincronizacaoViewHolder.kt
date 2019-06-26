355df62f8165af4c220c93df32cd3b0c19017ae7
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-26 15:37:59 -0300

    Implementando ViewFlippper

--- app/src/main/java/com/example/honeycomb/ui/googlePlay/SincronizacaoViewHolder.kt
+++ app/src/main/java/com/example/honeycomb/ui/googlePlay/SincronizacaoViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.honeycomb.ui.googlePlay
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.ViewFlipper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.honeycomb.R
 
@@ -12,10 +13,13 @@ class SincronizacaoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
         val nomeView = itemView.findViewById<TextView>(R.id.sincronizacao_item_model_nome)
         val descricaoView = itemView.findViewById<TextView>(R.id.sincronizacao_item_model_descricao)
         val iconeView = itemView.findViewById<ImageView>(R.id.sincronizacao_item_model_icone)
+        val statusView = itemView.findViewById<ViewFlipper>(R.id.sincronizacao_item_model_status)
 
         nomeView.text = item.nome
         descricaoView.text = item.descricao
         iconeView.setImageResource(item.icone)
+
+        statusView.displayedChild = item.status.ordinal
     }
 
 }
