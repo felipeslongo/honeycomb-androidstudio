f23d2431ce317b502899caadd525307384be4db5
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-28 20:18:45 -0300

    Implementing constructor AvatarView

--- app/src/main/java/com/example/honeycomb/ui/imageViews/AvatarView.kt
+++ app/src/main/java/com/example/honeycomb/ui/imageViews/AvatarView.kt
@@ -1,12 +1,30 @@
 package com.example.honeycomb.ui.imageViews
 
 import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
 import com.example.honeycomb.databinding.ViewAvatarBinding
 
-class AvatarView(private val binding: ViewAvatarBinding) {
+class AvatarView(private val avatarView: AppCompatImageView) {
+
+    companion object{
+        fun create(binding: ViewAvatarBinding) : AvatarView{
+            return AvatarView(binding.avatar)
+        }
+
+        fun create(avatarView: AppCompatImageView) : AvatarView{
+            return AvatarView(avatarView)
+        }
+
+        fun createWithBinding(rootView: View) : AvatarView{
+            val binding = ViewAvatarBinding.bind(rootView)
+            return create(binding)
+        }
+    }
+
     var drawable: Drawable
-        get() = binding.avatar.drawable
+        get() = avatarView.drawable
         set(value) {
-            binding.avatar.setImageDrawable(value)
+            avatarView.setImageDrawable(value)
         }
 }
