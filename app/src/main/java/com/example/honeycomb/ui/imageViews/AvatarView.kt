112f879106048149c6677a655181815ff31e2274
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-06-30 09:42:35 -0300

    companion.create

--- app/src/main/java/com/example/honeycomb/ui/imageViews/AvatarView.kt
+++ app/src/main/java/com/example/honeycomb/ui/imageViews/AvatarView.kt
@@ -3,6 +3,7 @@ package com.example.honeycomb.ui.imageViews
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.example.honeycomb.databinding.ViewAvatarBinding
 
 
@@ -11,6 +12,11 @@ class AvatarView(private val binding: ViewAvatarBinding) {
     companion object{
         fun create(inflater: LayoutInflater) : AvatarView{
             val binding = ViewAvatarBinding.inflate(inflater)
+            return create(binding)
+        }
+
+        fun create(inflater: LayoutInflater, parent: ViewGroup) : AvatarView{
+            val binding = ViewAvatarBinding.inflate(inflater, parent, false)
             return create(binding)
         }
 
