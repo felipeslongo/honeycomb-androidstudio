283c986c454d2659266baf6770eb98d467167052
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-07-21 17:14:01 -0300

    executeUsecase2

--- app/src/main/java/com/example/honeycomb/kotlin/coroutines/RunBlockingUsecases.kt
+++ app/src/main/java/com/example/honeycomb/kotlin/coroutines/RunBlockingUsecases.kt
@@ -4,13 +4,19 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 class RunBlockingUsecases {
-    fun printHelloWorldWithVoidTaskUsecase(){
+    fun executeUsecase1(){
         println("Hello,")
 
         runBlocking {
             suspendableTasks().voidTask()
         }
 
+        println("World!")
+    }
+
+    fun executeUsecase2() = runBlocking {
+        println("Hello,")
+        suspendableTasks().voidTask()
         println("World!")
     }
 }
--- app/src/test/java/com/example/honeycomb/kotlin/coroutines/RunBlockingUsecasesTests.kt
+++ app/src/test/java/com/example/honeycomb/kotlin/coroutines/RunBlockingUsecasesTests.kt
@@ -3,19 +3,35 @@ package com.example.honeycomb.kotlin.coroutines
 import com.example.honeycomb.infrastructure.SystemOutListener
 import org.junit.Assert
 import org.junit.Test
-import java.io.PrintStream
-import java.util.*
 import kotlin.collections.ArrayList
 
 class RunBlockingUsecasesTests {
-    class printHelloWorldWithVoidTaskUsecaseTests{
+    class executeUsecase1Tests{
         @Test
         fun shouldExecuteSynchronously(){
             val outputs = ArrayList<String>()
             SystemOutListener.instance.subscribe { outputs.add(it) }
             val usecases = RunBlockingUsecases()
 
-            usecases.printHelloWorldWithVoidTaskUsecase()
+            usecases.executeUsecase1()
+
+            val expectedOutputs = arrayListOf(
+                "Hello,",
+                "start voidTask",
+                "end voidTask",
+                "World!")
+            Assert.assertArrayEquals(expectedOutputs.toArray(), outputs.toArray())
+        }
+    }
+
+    class executeUsecase2Tests{
+        @Test
+        fun shouldExecuteSynchronously(){
+            val outputs = ArrayList<String>()
+            SystemOutListener.instance.subscribe { outputs.add(it) }
+            val usecases = RunBlockingUsecases()
+
+            usecases.executeUsecase2()
 
             val expectedOutputs = arrayListOf(
                 "Hello,",
