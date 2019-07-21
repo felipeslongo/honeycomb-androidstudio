9d77010fe4c30011d178bfc1ce775a92484621b6
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-07-21 18:54:57 -0300

    Update RunBlockingUsecases.kt

--- app/src/main/java/com/example/honeycomb/kotlin/coroutines/RunBlockingUsecases.kt
+++ app/src/main/java/com/example/honeycomb/kotlin/coroutines/RunBlockingUsecases.kt
@@ -6,6 +6,12 @@ import kotlinx.coroutines.runBlocking
 import kotlin.coroutines.CoroutineContext
 
 class RunBlockingUsecases {
+    /**
+     * runBlocking is a coroutine function. By not providing any context,
+     * it will get run on the mainThread.
+     * Hence the result as running Thread.run(). Essentially the code is run in synchronous in a
+     * sequential manner. (i.e. code run per the order of the line).
+     */
     fun executeNestedWithoutContextRunBlocking(){
         runBlocking {
             suspendableTasks().voidTask()
@@ -21,6 +27,11 @@ class RunBlockingUsecases {
         CoroutineLogger.println("World!")
     }
 
+    /**
+     * Providing a CommonPool context to runBlocking, enable it to trigger a new thread in it’s run.
+     * The interesting part of this code is, it is making the asynchronous code run in sequential
+     * manner, where the original function code will only get run after runBlocking completed it’s code.
+     */
     fun executeNestedWithCommonPoolContextRunBlocking(){
         runBlocking(Dispatchers.Default) {
             suspendableTasks().voidTask()
