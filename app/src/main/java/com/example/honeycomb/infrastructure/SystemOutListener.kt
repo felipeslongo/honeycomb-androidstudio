239a7f70d270e940194f99e1ca3c5c02c94cb6c3
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-07-21 16:46:46 -0300

    Create SystemOutListener.kt

---
+++ app/src/main/java/com/example/honeycomb/infrastructure/SystemOutListener.kt
@@ -0,0 +1,50 @@
+package com.example.honeycomb.infrastructure
+
+import java.io.PrintStream
+
+class SystemOutListener {
+
+    private val observers: ArrayList<(String) -> Unit> = arrayListOf()
+
+    fun subscribe(observer: (String) -> Unit){
+        observers.add(observer)
+    }
+
+    fun unsubscribe(observer: (String) -> Unit){
+        observers.remove(observer)
+    }
+
+    private fun notifyObservers(message: String?){
+        if(message.isNullOrEmpty()){
+            return
+        }
+
+        for (observer in observers){
+            observer(message)
+        }
+    }
+
+    private fun replaceStandardSystemOutPrintStream(){
+        System.setOut(PrintLnListener(this))
+    }
+
+    companion object{
+        val instance: SystemOutListener by lazy {
+            val listener = SystemOutListener()
+            listener.replaceStandardSystemOutPrintStream()
+            listener
+        }
+    }
+
+    private class PrintLnListener(val systemOutListener: SystemOutListener) : PrintStream(System.out){
+        override fun println(x: String?) {
+            super.println(x)
+            systemOutListener.notifyObservers(x)
+        }
+
+        override fun print(s: String?) {
+            super.print(s)
+            systemOutListener.notifyObservers(s)
+        }
+    }
+}
