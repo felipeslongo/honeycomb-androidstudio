dfa5783c23d1ebb0b60c8dfcad98ebb81ef95583
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-07-21 18:02:13 -0300

    ThreadServices.name

--- app/src/main/java/com/example/honeycomb/infrastructure/ThreadServices.kt
+++ app/src/main/java/com/example/honeycomb/infrastructure/ThreadServices.kt
@@ -5,6 +5,9 @@ import android.os.Looper
 
 class ThreadServices {
     companion object{
+        val name: String
+            get() =  Thread.currentThread().name
+
         fun isMain(): Boolean{
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 return Looper.getMainLooper().isCurrentThread
