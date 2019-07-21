099bcaf9ee93b07a1dc1fd674f16410b981f1119
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-07-21 17:36:04 -0300

    Create ThreadServices.kt

---
+++ app/src/main/java/com/example/honeycomb/infrastructure/ThreadServices.kt
@@ -0,0 +1,20 @@
+package com.example.honeycomb.infrastructure
+
+import android.os.Build
+import android.os.Looper
+
+class ThreadServices {
+    companion object{
+        fun isMain(): Boolean{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return Looper.getMainLooper().isCurrentThread
+            }
+
+            return Thread.currentThread() ==  Looper.getMainLooper().thread
+        }
+
+        fun isMainNoAndroid(): Boolean{
+            return Thread.currentThread().id == 1L
+        }
+    }
+}
