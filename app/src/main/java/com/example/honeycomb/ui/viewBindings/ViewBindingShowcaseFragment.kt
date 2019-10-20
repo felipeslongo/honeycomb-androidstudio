1fdbc82cb444e2ccadebae07c27b76434c16afd4
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-10-20 16:53:32 -0300

    Feature/View Binding (#10)
    * enable View Binding in project
    
    * add view binding to the navigation flow
    
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>
    
    * View Binding demo
    
    * using viewBindingIgnore but is not working as expected
    
    * showcase nullable views that binding is conditionally

---
+++ app/src/main/java/com/example/honeycomb/ui/viewBindings/ViewBindingShowcaseFragment.kt
@@ -0,0 +1,38 @@
+package com.example.honeycomb.ui.viewBindings
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.honeycomb.R
+import com.example.honeycomb.databinding.ViewBindingShowcaseFragmentBinding
+
+class ViewBindingShowcaseFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ViewBindingShowcaseFragment()
+    }
+
+    private lateinit var viewModel: ViewBindingShowcaseViewModel
+    private lateinit var binding: ViewBindingShowcaseFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ViewBindingShowcaseFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ViewBindingShowcaseViewModel::class.java)
+        binding.bindedView.text = viewModel.bindedView //Normal binding
+        binding.ignoredView.text = viewModel.ignoredView //An compilation error should have happened
+        binding.landscapeNullableView?.text = viewModel.landscapeNullableView //Only executes when it is not null. Its a View that exists conditionally
+    }
+
+}
