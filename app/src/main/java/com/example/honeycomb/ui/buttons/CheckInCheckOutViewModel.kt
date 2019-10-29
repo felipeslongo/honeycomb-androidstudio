b0a48d2f540e7d2d3bce862b14d9b6e848e3d6f8
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-10-29 18:13:32 -0300

    add and bind property textStringId

--- app/src/main/java/com/example/honeycomb/ui/buttons/CheckInCheckOutViewModel.kt
+++ app/src/main/java/com/example/honeycomb/ui/buttons/CheckInCheckOutViewModel.kt
@@ -6,9 +6,13 @@ import androidx.lifecycle.ViewModel
 import com.example.honeycomb.R
 
 class CheckInCheckOutViewModel : ViewModel() {
+    private val _textStringId: LiveData<Int> = MutableLiveData(R.string.check_in)
     private val _iconDrawableId: LiveData<Int> = MutableLiveData(R.drawable.ic_where_to_vote_24dp)
     private val _iconTint: LiveData<Int> = MutableLiveData(android.R.color.black)
     private val _isCheckedIn: LiveData<Boolean> = MutableLiveData(false)
+
+    val textStringId : LiveData<Int>
+        get() = _textStringId
 
     val iconDrawableId : LiveData<Int>
         get() = _iconDrawableId
