2fb99eae48e6d72b16667c6a917988a0ea138a18
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-10-29 17:30:47 -0300

    creating iconTint and isCheckedIn properties in ViewModel

--- app/src/main/java/com/example/honeycomb/ui/buttons/CheckInCheckOutViewModel.kt
+++ app/src/main/java/com/example/honeycomb/ui/buttons/CheckInCheckOutViewModel.kt
@@ -7,7 +7,15 @@ import com.example.honeycomb.R
 
 class CheckInCheckOutViewModel : ViewModel() {
     private val _iconDrawableId: LiveData<Int> = MutableLiveData(R.drawable.ic_where_to_vote_24dp)
+    private val _iconTint: LiveData<Int> = MutableLiveData(android.R.color.black)
+    private val _isCheckedIn: LiveData<Boolean> = MutableLiveData(false)
 
     val iconDrawableId : LiveData<Int>
         get() = _iconDrawableId
+
+    val iconTint : LiveData<Int>
+        get() = _iconTint
+
+    val isCheckedIn : LiveData<Boolean>
+        get() = _isCheckedIn
 }
