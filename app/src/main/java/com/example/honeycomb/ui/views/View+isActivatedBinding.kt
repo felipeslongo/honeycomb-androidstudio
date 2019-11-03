339f1229e8a10f8a65cba45cf377f572db239260
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-03 17:59:24 -0300

    rename to isActivatedBinding

--- app/src/main/java/com/example/honeycomb/ui/views/View+isActivatedBinding.kt
+++ app/src/main/java/com/example/honeycomb/ui/views/View+isActivatedBinding.kt
@@ -3,7 +3,7 @@ package com.example.honeycomb.ui.views
 import android.view.View
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("app:isActivated")
-fun isActivated(view: View, state: Boolean){
+@BindingAdapter("app:isActivatedBinding")
+fun isActivatedBinding(view: View, state: Boolean){
     view.isActivated = state
 }
