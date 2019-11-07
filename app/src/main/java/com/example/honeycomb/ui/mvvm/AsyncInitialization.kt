0147f1330e987e8bd640137a1419b7590a148989
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-07 14:42:02 -0300

    Create AsyncInitialization.kt

---
+++ app/src/main/java/com/example/honeycomb/ui/mvvm/AsyncInitialization.kt
@@ -0,0 +1,16 @@
+package com.example.honeycomb.ui.mvvm
+
+import kotlinx.coroutines.Job
+
+/**
+ * The Asynchronous Initialization Pattern
+ * Marks a type as requiring asynchronous initialization
+ * and provides the result of that initialization.
+ * @see https://blog.stephencleary.com/2013/01/async-oop-2-constructors.html
+ */
+interface AsyncInitialization {
+    /**
+     * The result of the asynchronous initialization of this instance.
+     */
+    val Initialization : Job
+}
