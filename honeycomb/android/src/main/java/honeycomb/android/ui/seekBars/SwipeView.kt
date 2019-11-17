5be631ed3dd2f68ee99c24c5d73bb1d9b5c82126
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-17 18:36:29 -0300

    Add empty listener to detect swipe changes in SwipeView

--- honeycomb/android/src/main/java/honeycomb/android/ui/seekBars/SwipeView.kt
+++ honeycomb/android/src/main/java/honeycomb/android/ui/seekBars/SwipeView.kt
@@ -1,11 +1,40 @@
 package honeycomb.android.ui.seekBars
 
+import android.widget.SeekBar
 import honeycomb.android.R
 import honeycomb.android.databinding.ViewSwipeBinding
 import honeycomb.android.ui.dataBindings.ViewFactory
+import honeycomb.platform.android.widget.getProgressPercentage
 
 class SwipeView(val binding: ViewSwipeBinding) {
     val viewModel = binding.viewModel!!
+
+    init {
+        binding.viewSwipeSeekbar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+//                if (progress > 95) {
+//                    seekBar?.thumb = seekBar!!.resources.getDrawable(R.drawable.ic_where_to_vote_32dp)
+//                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar == null)
+                    return
+
+                if (seekBar.getProgressPercentage() < 0.90) {
+                    seekBar.progress = 0
+                    return
+                }
+
+                seekBar.progress = seekBar.max
+            }
+        })
+    }
 
     companion object {
         val factory = ViewFactory<ViewSwipeBinding, SwipeView>(R.id.view_movingbackground) {
