e3a9dd4a714f1f6b19df7b454941a25bca5967f7
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-18 01:36:13 -0300

    Update PercentageTest.kt

--- honeycomb/kotlin/src/test/java/honeycomb/domain/valuetypes/PercentageTest.kt
+++ honeycomb/kotlin/src/test/java/honeycomb/domain/valuetypes/PercentageTest.kt
@@ -1,11 +1,14 @@
 package honeycomb.domain.valuetypes;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 
 class PercentageTest {
+    private lateinit var _percentage50: Percentage
 
     @BeforeEach
     fun setUp() {
+        _percentage50 = Percentage(0.5f)
     }
 
     @AfterEach
@@ -17,7 +20,10 @@ class PercentageTest {
     }
 
     @Test
-    fun isLessThan() {
+    fun isLessThan1() {
+        val actual = _percentage50.isLessThan(Percentage.Percent.V040)
+
+        assertFalse(actual)
     }
 
     @Test
