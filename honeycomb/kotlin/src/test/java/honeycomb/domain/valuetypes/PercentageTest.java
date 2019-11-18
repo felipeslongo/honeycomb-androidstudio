24dfb1406e18c45ad0251c421fd3700158f46077
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-18 01:26:06 -0300

    Add support for JUnit5 in honeycomb:kotlin gradle.config

---
+++ honeycomb/kotlin/src/test/java/honeycomb/domain/valuetypes/PercentageTest.java
@@ -0,0 +1,34 @@
+package honeycomb.domain.valuetypes;
+
+import org.junit.jupiter.api.*;
+
+class PercentageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void isLessThan() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void component1() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
