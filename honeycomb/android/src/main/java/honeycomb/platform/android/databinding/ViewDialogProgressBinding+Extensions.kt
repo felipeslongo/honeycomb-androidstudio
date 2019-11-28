2b3a032f64b59c71b329f1fe30aa918623ef8cf5
Felipe de Souza Longo <felipeslongo@gmail.com>
2019-11-28 10:53:01 -0300

    Remove ViewDialogProgressBinding+Extensions.kt
    Signed-off-by: Felipe de Souza Longo <felipeslongo@gmail.com>

--- honeycomb/android/src/main/java/honeycomb/platform/android/databinding/ViewDialogProgressBinding+Extensions.kt
+++
@@ -1,7 +0,0 @@
-package honeycomb.platform.android.databinding
-
-import honeycomb.android.databinding.ViewDialogProgressBinding
-
-fun ViewDialogProgressBinding.setProgressText(progressText: String) {
-    viewDialogProgressText.text = progressText
-}
