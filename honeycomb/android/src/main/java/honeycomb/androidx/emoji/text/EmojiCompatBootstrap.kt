b3721875e99e53c01caf0c79e8c39ad51c1ff992
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-06-03 19:42:44 -0300

    Create workflow to request emoji font from google fonts

---
+++ honeycomb/android/src/main/java/honeycomb/androidx/emoji/text/EmojiCompatBootstrap.kt
@@ -0,0 +1,50 @@
+package honeycomb.androidx.emoji.text
+
+
+import android.content.Context
+import androidx.core.provider.FontRequest
+import androidx.emoji.text.EmojiCompat
+import androidx.emoji.text.FontRequestEmojiCompatConfig
+import honeycomb.android.R
+import honeycomb.kotlin.wrapWithException
+import kotlinx.coroutines.*
+import kotlin.Exception
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class EmojiCompatBootstrap() {
+
+    suspend fun bootstrapAsync(context: Context) = suspendCancellableCoroutine<Unit> { coroutine ->
+        val fontRequest = createEmojiFontRequest()
+        val config = FontRequestEmojiCompatConfig(context, fontRequest)
+        val emojiCallback = createFontRequestEmojiCompatConfigInitCallback(coroutine, config)
+        config.registerInitCallback(emojiCallback)
+        EmojiCompat.init(config)
+    }
+
+    private fun createEmojiFontRequest() = FontRequest(
+        "com.google.android.gms.fonts",
+        "com.google.android.gms",
+        "Noto Color Emoji Compat",
+        R.array.com_google_android_gms_fonts_certs
+    )
+
+    private fun createFontRequestEmojiCompatConfigInitCallback(
+        coroutine: CancellableContinuation<Unit>,
+        config: FontRequestEmojiCompatConfig) = object : EmojiCompat.InitCallback() {
+
+        override fun onInitialized() {
+            super.onInitialized()
+            coroutine.resume(Unit)
+            config.unregisterInitCallback(this)
+        }
+
+        override fun onFailed(throwable: Throwable?) {
+            super.onFailed(throwable)
+            val exception = throwable?.wrapWithException()
+                ?: Exception("EmojiCompat Initialization Failed")
+            coroutine.resumeWithException(exception)
+            config.unregisterInitCallback(this)
+        }
+    }
+}
